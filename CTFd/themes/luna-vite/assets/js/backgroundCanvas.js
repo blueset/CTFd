f7fb7ae63cf35003b549a9dd4579e0a5f7c9defe
Eana Hufwe <eana@1a23.com>
2023-08-22 21:57:41 -0700

    Firefox background animation perf optimization

--- CTFd/themes/luna-vite/assets/js/backgroundCanvas.js
+++ CTFd/themes/luna-vite/assets/js/backgroundCanvas.js
@@ -1,40 +1,56 @@
 const BackgroundCanvasResources = {
     canvas: null,
-    iconWall: null,
+    iconWallCanvas: null
 };
 
-function drawPatternScale(ctx, img, startx, starty, width, height, scale) {
+const rad50 = 0.872665;
+const rad40 = Math.PI / 2 - rad50;
+const scale = 0.5;
+
+function buildIconWallData(w, h, iconWall) {
+    const offscreenCanvas = document.createElement("canvas");
+    const offscreenCtx = offscreenCanvas.getContext("2d");
+
+    const a1 = w * Math.cos(rad40),
+        a2 = h * Math.cos(rad50),
+        b1 = h * Math.cos(rad40),
+        b2 = w * Math.cos(rad50);
+
+    const width = (b1 + b2) / scale;
+    const height = (a1 + a2 + 580) / scale;
+
+    offscreenCanvas.width = width;
+    offscreenCanvas.height = height;
+
+    const pattern = offscreenCtx.createPattern(iconWall, "repeat");
+    offscreenCtx.fillStyle = pattern;
+    offscreenCtx.fillRect(0, 0, width, height);
+    BackgroundCanvasResources.offscreenCanvas = offscreenCanvas;
+}
+
+function drawPatternScale(ctx, offscreenCanvas, startx, starty, width, height) {
     ctx.scale(scale, scale);
-
-    const triangularPattern = ctx.createPattern(img, "repeat");
-    ctx.fillStyle = triangularPattern;
-    ctx.fillRect(startx / scale, starty / scale, width / scale, height / scale);
-
+    ctx.drawImage(offscreenCanvas, startx / scale, starty / scale, width / scale, height / scale);
     ctx.restore();
 }
 
 function render(timer) {
     timer = (timer / 30) % 580;
-    const {canvas, iconWall} = BackgroundCanvasResources;
-    if (canvas && iconWall) {
+    const { canvas, offscreenCanvas } = BackgroundCanvasResources;
+    if (canvas && offscreenCanvas) {
         const ctx = canvas.getContext("2d");
         ctx.save();
         const w = canvas.width,
             h = canvas.height;
         ctx.clearRect(0, 0, w, h);
         ctx.save();
-        const rad50 = 0.872665;
-        const rad40 = Math.PI / 2 - rad50;
         ctx.rotate(rad50);
         const a1 = w * Math.cos(rad40),
             a2 = h * Math.cos(rad50),
             b1 = h * Math.cos(rad40),
             b2 = w * Math.cos(rad50);
         ctx.translate(0, -timer);
-        drawPatternScale(ctx, iconWall, 0, -a1, b1 + b2, a1 + a2 + 580, 0.5);
-
-        //ctx.font = "20px sans-serif";
-        //ctx.fillText(`Timer: ${timer}`, 10, 50);
+        drawPatternScale(ctx, offscreenCanvas, 0, -a1, b1 + b2, a1 + a2 + 580);
     }
 }
 
@@ -53,21 +69,24 @@ export default function IconWallCanvasInit() {
         canvas = document.createElement("canvas");
         canvas.id = "iconWallCanvas";
         canvas.classList.add("iconWallCanvas");
-        container.insertBefore( canvas, container.firstChild);
+        container.insertBefore(canvas, container.firstChild);
     }
     const iconWall = new Image();
+    iconWall.crossOrigin = "anonymous";
+    iconWall.onload = () => {
+        buildIconWallData(canvas.width, canvas.height, iconWall);
+    };
     iconWall.src = /(?:")(.+)(?=")/g.exec(getComputedStyle(document.documentElement).getPropertyValue("--icon-wall"))[1];
 
     BackgroundCanvasResources.canvas = canvas;
-    BackgroundCanvasResources.iconWall = iconWall;
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     window.addEventListener("resize", () => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+        buildIconWallData(canvas.width, canvas.height, iconWall);
     });
 
     window.requestAnimationFrame(frame);
-
 }
